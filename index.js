5f5e7bfc8230bd168ccf8f4f388a67ce792f5ff8
mannycolon <colonmanuel7@gmail.com>
2017-04-14 13:18:28 -0400

    fix port naming

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var app = express()
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
   res.send("Please go to the following pathname /api/whoami")
